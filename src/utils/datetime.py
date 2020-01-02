98462b4f80bc3033d613dc695607652392b59864
Miguel Dingli <miguel.dingli.15@um.edu.mt>
2020-01-02 14:51:08 +0100

    Function strfdelta now adds days to hours if days is not in the formatter.

--- src/utils/datetime.py
+++ src/utils/datetime.py
@@ -2,7 +2,15 @@ from datetime import timedelta
 
 
 def strfdelta(delta: timedelta, fmt: str) -> str:
-    d = {"days": delta.days}
+    # Get hours, minutes, seconds
+    d = {}
     d["hours"], rem = divmod(delta.seconds, 3600)
     d["minutes"], d["seconds"] = divmod(rem, 60)
+
+    # In the case of days, if not in fmt, add to hours
+    if "days" in fmt:
+        d["days"] = delta.days
+    else:
+        d["hours"] += delta.days * 24
+
     return fmt.format(**d)
