e379fabb65a07bcf8659bfb5cdff4c7720635a34
migueldingli1997 <miguel@simply-vc.com.mt>
2020-01-10 16:24:27 +0100

    Added missing setting.

--- src/utils/config_parsers/internal.py
+++ src/utils/config_parsers/internal.py
@@ -55,6 +55,8 @@ class InternalConfig(ConfigParser):
             section['redis_node_monitor_alive_key_timeout'])
         self.redis_network_monitor_alive_key_timeout = int(
             section['redis_network_monitor_alive_key_timeout'])
+        self.redis_network_monitor_last_height_key_timeout = int(
+            section['redis_network_monitor_last_height_key_timeout'])
 
         # [monitoring_periods]
         section = cp['monitoring_periods']
